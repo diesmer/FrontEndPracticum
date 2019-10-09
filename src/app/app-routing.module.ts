1dfa29af83a50444c72fdd3fc1e07d74d633a046
Diesmer Hensbergen <DiesmerHensbergen@reeleezee.nl>
2019-10-09 22:54:06 +0200

    Begin login (nodig voor switch naar eigen branch)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './Login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'login', component: LoginComponent }
+    // otherwise redirect to home
+];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
