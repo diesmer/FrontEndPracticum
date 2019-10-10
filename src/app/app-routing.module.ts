3f18b8ab60bbc5f3dd4cbbb2fd85a0b1c2573363
Diesmer Hensbergen <diesdies@hotmail.com>
2019-10-10 09:26:59 +0200

    VerzoekIndienen pagina Registreer pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './Login/login.component';
+import { RegistreerPaginaComponent } from './registreerPagina/registreer.component';
+import { VerzoekIndienenComponent } from './verzoekIndienen/verzoekIndienen.component';
 
 const routes: Routes = [
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'registreer', component: RegistreerPaginaComponent},
+    { path: 'verzoekIndienen', component: VerzoekIndienenComponent}
     // otherwise redirect to home
 ];
 
---
+++ src/app/verzoekIndienen/verzoekIndienen.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({ 
+    selector: 'verzoekIndienen',
+    templateUrl: 'verzoekIndienen.component.html',
+    styleUrls: ['./verzoekIndienen.component.css']
+ })
+export class VerzoekIndienenComponent {}
